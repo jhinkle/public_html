112bad63038ec21442197a2b0f3f9ba28995bebb
Jordan Hinkle <jordan.hinkle@gmail.com>
2013-01-28 16:35:11 -0600

    hover over text

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ navigation();
 ?>
     <div class="container">
         <h2>About</h2>
-        <p class="span4"><a href="#" class="thumbnail"><img src="http://placehold.it/360x270" alt="Handsome Devil"/></a></p>
+        <p class="span4"><a href="#" class="thumbnail"><img src="http://placehold.it/360x270" title="Handsom devil!" alt="Handsome Devil"/></a></p>
         <p class="span3">Coming soon... I promise!</p>
     </div>
 <?php
